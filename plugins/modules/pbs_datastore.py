4ac96d12e842e659ce5e4b83c0b43687e5ae4de5
Max Hösel <git@maxhoesel.de>
2022-03-09 03:09:29 +0100

    feat: add pbs modules

---
+++ plugins/modules/pbs_datastore.py
@@ -0,0 +1,239 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2022, Max Hösel <ansible@maxhoesel.de>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import (absolute_import, division, print_function)
+
+__metaclass__ = type
+
+DOCUMENTATION = r"""
+---
+module: pbs_datastore
+author: Max Hösel (@maxhoesel)
+short_description: Manage datastores on PBS
+version_added: '4.0.0'
+description: Create, update and delete datastores on a Proxmox Backup Server
+notes:
+  - Check mode is supported.
+options:
+  comment:
+    description: Comment for the datastore (visible in UI)
+    type: str
+  gc_schedule:
+    description: Run garbace collection job at specified schedule. Must be a calendar event (i.e C(daily), C(Tue 04:27))
+    type: str
+  keep_last:
+    description: Number of backups to keep
+    type: int
+  keep_hourly:
+    description: Number of hourly backups to keep
+    type: int
+  keep_daily:
+    description: Number of daily backups to keep
+    type: int
+  keep_weekly:
+    description: Number of weekly backups to keep
+    type: int
+  keep_monthly:
+    description: Number of monthly backups to keep
+    type: int
+  keep_yearly:
+    description: Number of yearly backups to keep
+    type: int
+  name:
+    description: Name of the datastore
+    type: str
+    required: yes
+  notify:
+    description: Datastore notification settings
+    type: dict
+    suboptions:
+      gc:
+        description: Garbage Collection notification setting
+        type: str
+        choices: ["always", "never", "errors"]
+      sync:
+        description: Sync Jobs notification setting
+        type: str
+        choices: ["always", "never", "errors"]
+      verify:
+        description: Verification notification setting
+        type: str
+        choices: ["always", "never", "errors"]
+  notify_user:
+    description: User ID to notify
+    type: str
+  path:
+    description: Backing path for the datastore. Required when creating a new datastore
+    type: str
+  prune_schedule:
+    description: Run prune job at specified schedule. Must be a calendar event (i.e C(daily), C(Tue 04:27))
+    type: str
+  state:
+    description: Whether this datastore should be C(present) or C(absent)
+    default: present
+    type: str
+    choices:
+      - present
+      - absent
+  verify_new:
+    description: If enabled, all new backups will be verified right after completion
+    type: bool
+
+extends_documentation_fragment:
+  - maxhoesel.proxmox.api_connection
+"""
+
+EXAMPLES = r"""
+- name: Create/Update a datastore
+  maxhoesel.proxmox.pbs_datastore:
+    name: test-datastore
+    path: /mnt/somedisk/thisdirectory
+    gc_schedule: daily
+    prune_schedule: daily
+    keep_daily: 7
+    keep_weekly: 4
+    keep_monthly: 6
+    notify:
+      gc: errors
+      verify: errors
+      sync: errors
+    verify_new: yes
+"""
+
+from ansible.module_utils.basic import AnsibleModule
+
+from ..module_utils.api_connection import api_connection_argspec, init
+
+try:
+    import proxmoxer
+    HAS_PROXMOXER = True
+except ImportError:
+    HAS_PROXMOXER = False
+
+
+def _make_datastore_params(module: AnsibleModule) -> dict:
+    notify = ",".join([key + "=" + module.params["notify"][key] for key in module.params["notify"]])
+    datastore_params = {
+        "name": module.params["name"],
+        "path": module.params["path"],
+        "comment": module.params["comment"],
+        "gc-schedule": module.params["gc_schedule"],
+        "keep-last": module.params["keep_last"],
+        "keep-hourly": module.params["keep_hourly"],
+        "keep-daily": module.params["keep_daily"],
+        "keep-weekly": module.params["keep_weekly"],
+        "keep-monthly": module.params["keep_monthly"],
+        "keep-yearly": module.params["keep_yearly"],
+        "notify-user": module.params["notify_user"],
+        "notify": notify,
+        "prune_schedule": module.params["prune_schedule"],
+        "verify-new": module.params["verify_new"]
+    }
+    datastore_params = {key: datastore_params[key]
+                        for key in datastore_params if datastore_params[key] is not None}
+    return datastore_params
+
+
+def add_datastore(module: AnsibleModule, proxmox, result: dict) -> dict:
+    datastore = _make_datastore_params(module)
+
+    if "path" not in datastore:
+        result["msg"] = "path is required to create a new datastore"
+        module.fail_json(**result)
+
+    try:
+        proxmox.config.datastore.post(**datastore)
+    except proxmoxer.ResourceException as e:
+        result["msg"] = "Could not create datastore. Exception: {0}".format(e)
+        module.fail_json(**result)
+    result["changed"] = True
+    return result
+
+
+def update_datastore(module: AnsibleModule, proxmox, result: dict) -> dict:
+    datastore = _make_datastore_params(module)
+
+    try:
+        getattr(proxmox.config.datastore, module.params["name"]).put(**datastore)
+    except proxmoxer.ResourceException as e:
+        result["msg"] = "Could not update datastore. Exception: {0}".format(e)
+        module.fail_json(**result)
+    result["changed"] = True
+
+    return result
+
+
+def delete_datastore(module: AnsibleModule, proxmox, result: dict) -> dict:
+    datastore = _make_datastore_params(module)
+    try:
+        proxmox.access.acl.put(**datastore)
+    except proxmoxer.ResourceException as e:
+        result["msg"] = "Could not delete datastore. Exception: {0}".format(e)
+        module.fail_json(**result)
+    result["changed"] = True
+    return result
+
+
+def main():
+
+    module_args = dict(
+        comment=dict(type="str"),
+        gc_schedule=dict(type="str"),
+        keep_last=dict(type="int"),
+        keep_hourly=dict(type="int"),
+        keep_daily=dict(type="int"),
+        keep_weekly=dict(type="int"),
+        keep_monthly=dict(type="int"),
+        keep_yearly=dict(type="int"),
+        name=dict(type="str", required=True),
+        notify=dict(type="dict", options=dict(
+            gc=dict(type="str", choices=["always", "never", "errors"]),
+            sync=dict(type="str", choices=["always", "never", "errors"]),
+            verify=dict(type="str", choices=["always", "never", "errors"])
+        )),
+        notify_user=dict(type="str"),
+        path=dict(type="str"),
+        prune_schedule=dict(type="str"),
+        state=dict(type="str", choices=["present", "absent"], default="present"),
+        verify_new=dict(type="bool")
+    )
+    result = dict(changed=False, msg="")
+    module = AnsibleModule(
+        argument_spec={**module_args, **api_connection_argspec},
+        supports_check_mode=True
+    )
+
+    proxmox = init(module, result, "PBS")
+
+    try:
+        datastores = proxmox.config.datastore.get()
+    except proxmoxer.ResourceException as e:
+        result["msg"] = "Could not get list of datastores. Exception: {0}".format(e)
+        module.fail_json(**result)
+
+    datastores_by_name = {ds["name"]: ds for ds in datastores}
+    datastore_exists = module.params["name"] in datastores_by_name
+
+    action = None
+    if not datastore_exists and module.params["state"] == "present":
+        action = add_datastore
+    elif datastore_exists and module.params["state"] == "absent":
+        action = delete_datastore
+    elif datastore_exists and module.params["state"] == "present":
+        params = _make_datastore_params(module)
+        for param in params:
+            if datastores_by_name[module.params["name"]].get(param, None) != params[param]:
+                action = update_datastore
+
+    if action is not None:
+        if module.check_mode:
+            result["changed"] = True
+        else:
+            result = action(module, proxmox, result)
+    module.exit_json(**result)
+
+
+if __name__ == "__main__":
+    main()
---
+++ plugins/modules/pbs_token.py
@@ -0,0 +1,180 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2022, Max Hösel <ansible@maxhoesel.de>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import (absolute_import, division, print_function)
+
+__metaclass__ = type
+
+DOCUMENTATION = r"""
+---
+module: pbs_token
+author: Max Hösel (@maxhoesel)
+short_description: Manage user tokens on a Proxmox Backup Server
+version_added: '4.0.0'
+description: Create, update and delete user API tokens on a Proxmox backup Server instance.
+notes:
+  - Check mode is supported.
+options:
+  comment:
+    description: Comment for the token
+    type: str
+  enabled:
+    description: Whether to enable the token. false means disabled.
+    type: bool
+  expire:
+    description: Token expiration data (seconds since UNIX epoch). 0 means no expiration date
+    type: int
+  name:
+    description: Name of the token that will be part of the authid. Example (token1 => "user@pbs!token1")
+    aliases:
+      - token_name
+    required: yes
+    type: str
+  state:
+    description: C(present) makes sure the user exists, C(absent) makes sure the user is removed.
+    default: present
+    type: str
+    choices:
+      - present
+      - absent
+  userid:
+    description: User that the token will be created for. Must be in user@realm format (e.g. john@pbs)
+    required: yes
+    type: str
+
+extends_documentation_fragment:
+  - maxhoesel.proxmox.api_connection
+"""
+
+EXAMPLES = r"""
+- name: Create an API token for a user
+  maxhoesel.proxmox.pbs_token:
+    name: mytoken
+    userid: john@pbs
+"""
+
+RETURN = r"""
+tokenid:
+  description: API Token identifier
+  returned: always
+  type: str
+secret:
+  description: API Token secret
+  returned: When the token is created
+  type: str
+"""
+try:
+    import proxmoxer
+except ImportError:
+    pass  # Handled in init()
+
+from ansible.module_utils.basic import AnsibleModule
+
+from ..module_utils.api_connection import api_connection_argspec, init
+
+
+def _make_token_params(module: AnsibleModule) -> dict:
+    token_params = {
+        "comment": module.params["comment"],
+        "enable": module.params["enabled"],
+        "expire": module.params["expire"],
+    }
+    token_params = {key: token_params[key] for key in token_params if token_params[key] is not None}
+    return token_params
+
+
+def add_token(module: AnsibleModule, proxmox, result: dict) -> dict:
+    token = _make_token_params(module)
+
+    try:
+        r = getattr(getattr(proxmox.access.users, module.params["userid"]).token,
+                    module.params["name"]).post(**token)
+    except proxmoxer.ResourceException as e:
+        result["msg"] = "Could not create token. Exception: {0}".format(e)
+        module.fail_json(**result)
+    result["tokenid"] = r["tokenid"]
+    result["secret"] = r["value"]
+
+    result["changed"] = True
+    return result
+
+
+def update_token(module: AnsibleModule, proxmox, result: dict) -> dict:
+    token = _make_token_params(module)
+
+    try:
+        getattr(getattr(proxmox.access.users, module.params["userid"]).token,
+                module.params["name"]).put(**token)
+    except proxmoxer.ResourceException as e:
+        result["msg"] = "Could not update token. Exception: {0}".format(e)
+        module.fail_json(**result)
+
+    result["changed"] = True
+    return result
+
+
+def delete_token(module: AnsibleModule, proxmox, result: dict) -> dict:
+    try:
+        getattr(getattr(proxmox.access.users, module.params["userid"]).token,
+                module.params["name"]).delete()
+    except proxmoxer.ResourceException as e:
+        result["msg"] = "Could not delete token. Exception: {0}".format(e)
+        module.fail_json(**result)
+
+    result["changed"] = True
+    return result
+
+
+def main():
+    module_args = dict(
+        comment=dict(type="str"),
+        enabled=dict(type="bool"),
+        expire=dict(type="int"),
+        name=dict(type="str", required=True, aliases=["token_name"]),
+        state=dict(type="str", choices=["present", "absent"], default="present"),
+        userid=dict(type="str", required=True)
+    )
+    result = dict(changed=False, msg="")
+    module = AnsibleModule(
+        argument_spec={**module_args, **api_connection_argspec},
+        supports_check_mode=True
+    )
+
+    proxmox = init(module, result, "PBS")
+
+    try:
+        tokenlist = getattr(proxmox.access.users, module.params["userid"]).token.get()
+    except proxmoxer.ResourceException as e:
+        result["msg"] = "Could not get list tokens for user {0}. Exception: {1}".format(
+            module.params["userid"], e)
+        module.fail_json(**result)
+
+    tokens_by_name = {token["token-name"]: token for token in tokenlist}
+    token_exists = module.params["name"] in tokens_by_name
+
+    action = None
+    if not token_exists and module.params["state"] == "present":
+        action = add_token
+    elif token_exists and module.params["state"] == "absent":
+        action = delete_token
+    elif token_exists and module.params["state"] == "present":
+        if module.params["password_update"]:
+            action = update_token
+        else:
+            params = _make_token_params(module)
+            for param in params:
+                if tokens_by_name[module.params["userid"]].get(param, None) != params[param]:
+                    action = update_token
+
+    if action is not None:
+        if module.check_mode:
+            result["changed"] = True
+        else:
+            result = action(module, proxmox, result)
+    module.exit_json(**result)
+
+
+if __name__ == "__main__":
+    main()
