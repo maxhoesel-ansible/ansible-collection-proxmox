c9eeeb5e436091c4ecb33bceec29f0587fa63f1e
Max Hösel <git@maxhoesel.de>
2022-05-27 22:06:18 +0200

    switch to new CI and dev workflow

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,18 +2,16 @@
 set -eu
 set -o pipefail
 
-# Lint ansible roles and git commits
-tox -e lint
-
 # Sanity checks for our modules
-## --docker isn't technically required as tox-ansible autodetects it,
-## however, this prevents it from falling back to a venv and potentially modifying the local environment
 tox -e sanity -- --docker --color -v --python 3.6
 
-# Integration tests for modules - we don't have an unit tests as of now
-## --docker isn't technically required as tox-ansible autodetects it,
-## however, this prevents it from falling back to a venv and potentially modifying the local environment
-#tox -e integration -- --docker --color -v --python 3.6 --docker-terminate success
+# Integration tests for modules
+#tox -e integration -- --color -v --controller docker:default --target docker:centos7,seccomp=unconfined,privileged=yes
+#tox -e integration -- --color -v --controller docker:default --target docker:default,python=3.6
 
 # Molecule tests - this grabs all molecule scenarios and executes them.
-#tox -e "$(tox -l | grep ansible | grep -v "lint" | tr '\n' ',')"
+# Note that to run this command, you need to have
+# - run and tox command at least once (tox -l is fine)
+# - have the collection built and installed locally.
+#   Running the module tests will do that for you
+tox -e "$(tox -l | grep ansible | grep -v "lint" | tr '\n' ',')"
