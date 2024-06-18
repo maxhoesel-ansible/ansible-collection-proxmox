05cd54935bc3c57d61119cfe040de1fce09aa166
Max Hösel <git@maxhoesel.de>
2024-06-18 13:39:35 +0200

    bump ansible to 2.17 and python to 3.6
    with the ansible-2.17 release, python 2 and 3.6 have been removed
    from the officially supported list. We therefore have to upgrade
    our minimum tested version to 3.7, which is a breaking change,
    even if we are not planning on breaking support ourselves.

--- tests/plugins/test_plugins.py
+++ tests/plugins/test_plugins.py
@@ -1,10 +1,18 @@
 # pylint: disable=redefined-outer-name
 
 def test_plugins_sanity(collection_test_env, test_versions):
-    collection_test_env.run([
+    params = [
         "ansible-test",
         "sanity", "--docker", "--color", "-v",
         "--python", test_versions.node_python_version,
-        "--skip-test", "metaclass-boilerplate",
-        "--skip-test", "future-import-boilerplate",
-    ])
+    ]
+
+    if int(test_versions.ansible_version.split(".")[1]) <= 16:
+        # these flags are only valid with ansible-test 2.16 and older, as they refer to python 2.7.
+        # Do not include them with newer versions of ansible-test
+        params.extend([
+            "--skip-test", "metaclass-boilerplate",
+            "--skip-test", "future-import-boilerplate"
+        ])
+
+    collection_test_env.run(params)
